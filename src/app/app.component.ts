431d961b32b1e01bc52142facaeb29d2b052b376
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-08-28 10:32:51 +0200

    ajout images composant réutilisable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  mySecondSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'Trop mignon, trop gentil et surtout très utile dans notre vie',
+      'https://cdn.pixabay.com/photo/2023/05/19/13/28/bird-8004544_1280.jpg',
+      new Date(),
+      0
+    );
+    this.mySecondSnap = new FaceSnap(
+      'Star Wars',
+      'Oh oui je suis un grand fan, autant le montrer',
+      'https://pixabay.com/get/ga158241ca4afa364a46790a2bb7f00b822d5d6591ac2dc0bd139450c931c0a87bde30cc5f92969faeb89b3447400a494c297b79a177500793c0aef6dfe0d3937_1280.jpg',
+      new Date(),
+      0
+    );
+    this.myLastSnap = new FaceSnap(
+      'Escalade',
+      'Voici une de mes passions aussi',
+      'https://pixabay.com/get/gb179f6a146e8762ecda888105ba26880608fcca72bc5e08b2837c073b3cb390b2e10386c01327351bc805cfd8238f0a8bacbbf2d8f3d3d228bfdc23857f18654_1280.jpg',
+      new Date(),
+      0
+    );
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,29 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss'],
 })
 export class FaceSnapComponent implements OnInit {
-  imgUrl!: string;
-  title!: string;
-  description!: string;
-  creationDate!: Date;
-  snaps!: number;
+  @Input() faceSnap!: FaceSnap;
+
   buttonText!: string;
 
   ngOnInit() {
-    this.title = 'Archibald';
-    this.description = 'Super personne, trop gentil';
-    this.creationDate = new Date();
-    this.snaps = 7;
-    this.imgUrl =
-      'https://cdn.pixabay.com/photo/2023/05/19/13/28/bird-8004544_1280.jpg';
     this.buttonText = 'Oh yes Snap!';
   }
 
   onSnap() {
     if (this.buttonText === 'Oh yes Snap!') {
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.buttonText = 'Hum! unSnap!';
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.buttonText = 'Oh yes Snap!';
     }
   }
